6c5bb99fa40590432e78b406eb1e8decb8d650b7
Venax <81310818+venaxyt@users.noreply.github.com>
2021-06-27 19:43:10 +0200

    Update View Adder.py

--- View Adder.py
+++ View Adder.py
@@ -1,10 +1,12 @@
 import threading, random, os
-os.system("title 𝙂𝙄𝙏𝙃𝙐𝘽 𝙋𝙍𝙊𝙁𝙄𝙇𝙀 𝙑𝙄𝙀𝙒 𝘼𝘿𝘿𝙀𝙍"); os.system("cls")
+os.system("title 𝙂𝙄𝙏𝙃𝙐𝘽 𝙋𝙍𝙊𝙁𝙄𝙇𝙀 𝙑𝙄𝙀𝙒 𝘼𝘿𝘿𝙀𝙍")
+
 try:
     import requests
 except:
     os.system("py -m pip install requests")
     import requests
+os.system("cls")
 
 # Example of link : https://camo.githubusercontent.com/0464ddf5c8b77ac9f8f09e523218f34b1b1232d1b2488de484d0bb9b8521fd6f/68747470733a2f2f70726f66696c652d636f756e7465722e676c697463682e6d652f25374276656e617879742537442f636f756e742e737667
 link = input(" \033[94m[>] \033[38;2;100;90;255mProfile view plug-in link : ")
